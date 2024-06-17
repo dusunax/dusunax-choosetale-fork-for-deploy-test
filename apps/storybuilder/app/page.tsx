e272cfaa72e320c94d64173218315a6a35453f84
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-06-18 00:38:03 +0900

    chore: init shadcn UI, fix dev ports

--- apps/storybuilder/app/page.tsx
+++ apps/storybuilder/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { Card } from "@repo/ui/card";
+import { Button } from "@repo/ui/components/ui/button.jsx";
 import Link from "next/link";
 
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <Card title="">
+      <Button title="">
         <Link href="story/create">storybuilder</Link>
-      </Card>
+      </Button>
     </main>
   );
 }
