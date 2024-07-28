6616520524af7f9802d37a88ac38087428deff5d
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-07-28 23:14:26 +0900

    fix(S1): 게임 create 버튼 동작 (#48)

--- apps/game-builder/src/components/button/SubmitButton.tsx
+++ apps/game-builder/src/components/button/SubmitButton.tsx
@@ -1,13 +1,8 @@
-import type { MouseEvent } from "react";
 import ThemedButton from "@themed/ThemedButton";
 
 export default function NextButton({ text }: { text: string }) {
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
-
   return (
-    <ThemedButton className="w-full" type="submit" onClick={handleClick}>
+    <ThemedButton className="w-full" type="submit">
       {text}
     </ThemedButton>
   );
