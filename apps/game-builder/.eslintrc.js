2d930388dd51764b32a0ea64c9449a85bfc631e1
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-08-25 19:08:42 +0900

    fix(S1): eslint 수정

--- apps/game-builder/.eslintrc.js
+++ apps/game-builder/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ["@repo/eslint-config/next.js"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project: ["packages/ui"],
+      },
+    },
+  },
 };
