811875126ef00a6a93be615d4f76314dfe89abc5
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-10-19 01:23:55 +0900

    feat: 완료한 게임 결과 페이지 임시링크 추가

--- apps/game-builder/src/app/(my-page)/my-page/ended-game/@date/_components/GroupDateEndedGameCard.tsx
+++ apps/game-builder/src/app/(my-page)/my-page/ended-game/@date/_components/GroupDateEndedGameCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { type EndedGameGroupDate } from "@/interface/customType";
 import { useTranslation } from "@/hooks/useTranslation";
 import { formatDateString } from "@/utils/formatDatestring";
@@ -12,27 +13,32 @@ export default function GroupDateEndedGameCard({
   endedGame,
 }: EndedGameCardProps) {
   const { t } = useTranslation();
+  const TEMP_PLAY_ID = 1;
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="relative w-full pb-[100%] rounded-md overflow-hidden bg-grey-200">
-        <ErrorHandlingImage
-          src={endedGame.game.thumbnail.url}
-          alt={endedGame.game.title}
-        />
-      </div>
+    <Link
+      href={`/game-play/${TEMP_PLAY_ID}/result?gameId=${endedGame.game.id}`}
+    >
+      <div className="flex flex-col gap-2">
+        <div className="relative w-full pb-[100%] rounded-md overflow-hidden bg-grey-200">
+          <ErrorHandlingImage
+            src={endedGame.game.thumbnail.url}
+            alt={endedGame.game.title}
+          />
+        </div>
 
-      <div className="h-[88px] flex flex-col justify-between">
-        <div>
-          <p className="headline line-clamp-2">{endedGame.game.title}</p>
-          <p className="caption text-grey-200 text-thin">
-            {t(`genre.${endedGame.game.genre}`)}
+        <div className="h-[88px] flex flex-col justify-between">
+          <div>
+            <p className="headline line-clamp-2">{endedGame.game.title}</p>
+            <p className="caption text-grey-200 text-thin">
+              {t(`genre.${endedGame.game.genre}`)}
+            </p>
+          </div>
+          <p className="caption text-thin">
+            {formatDateString(endedGame.game.reachedEndingAt)}
           </p>
         </div>
-        <p className="caption text-thin">
-          {formatDateString(endedGame.game.reachedEndingAt)}
-        </p>
       </div>
-    </div>
+    </Link>
   );
 }
