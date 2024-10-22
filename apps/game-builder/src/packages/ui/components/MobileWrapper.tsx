aec9ce9d3541e1d0d95cd3ba38b5b21c5e5b9b1d
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-10-23 04:02:18 +0900

    feat(S8): webview navbar 출력 조건 추가

--- apps/game-builder/src/packages/ui/components/MobileWrapper.tsx
+++ apps/game-builder/src/packages/ui/components/MobileWrapper.tsx
@@ -1,17 +1,32 @@
 "use client";
-import { PropsWithChildren } from "react";
-import NavBar from "./NavBar";
+import { PropsWithChildren, useEffect, useState } from "react";
+import WebViewNavBar from "./NavBar";
 
 export default function MobileWrapper({
   children,
   hasNavBar = true,
 }: PropsWithChildren<{ hasNavBar?: boolean }>) {
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(hover: none) and (pointer: coarse)");
+
+    const checkTouchDevice = (e: MediaQueryListEvent) => {
+      setIsTouchDevice(e.matches);
+    };
+
+    setIsTouchDevice(mediaQuery.matches);
+    mediaQuery.addEventListener("change", checkTouchDevice);
+
+    return () => mediaQuery.removeEventListener("change", checkTouchDevice);
+  }, []);
+
   return (
     <div className="w-full h-[calc(100vh-0.1px)] flex bg-slate-200">
       <UnSupported />
       <div className="mobile-layout max-w-5xl flex flex-col justify-between mx-auto bg-white">
         <div className="flex-1 overflow-y-auto">{children}</div>
-        {hasNavBar && <NavBar />}
+        {hasNavBar && !isTouchDevice && <WebViewNavBar />}
       </div>
     </div>
   );
