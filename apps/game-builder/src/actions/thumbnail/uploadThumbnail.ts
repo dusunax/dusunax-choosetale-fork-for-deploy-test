3c3afe5cf1d4efcabdb3a21bdc6c86484f5dd773
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-10-25 23:51:22 +0900

    fix(S8): 썸네일 이미지 업로드 오류 수정

--- apps/game-builder/src/actions/thumbnail/uploadThumbnail.ts
+++ apps/game-builder/src/actions/thumbnail/uploadThumbnail.ts
@@ -2,6 +2,7 @@
 import { revalidateTag } from "next/cache";
 import type { HttpError } from "@choosetale/nestia-type";
 import api from "@/lib/axios/axios";
+import { API_URL } from "@/config/config";
 import type { ApiResponse, SuccessResponse } from "../action";
 
 interface UploadThumbnail {
@@ -20,9 +21,15 @@ export const uploadThumbnail = async (
   images: FormData
 ): Promise<ApiResponse<UploadThumbnailSuccessResponse>> => {
   try {
-    const response = await api.post(`/game/${gameId}/upload-thumbnail`, {
-      data: images,
-    });
+    const response = await api.post(
+      `/game/${gameId}/upload-thumbnail`,
+      images,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
 
     if (!response.data) {
       // const errorData = (await response.json()) as { message: string };
@@ -35,7 +42,10 @@ export const uploadThumbnail = async (
     }
 
     revalidateTag("game-info");
-    return { success: true, uploadedThumbnail: data[0] };
+    return {
+      success: true,
+      uploadedThumbnail: { ...data[0], url: API_URL + data[0].url },
+    };
   } catch (error) {
     return { success: false, error: error as HttpError };
   }
